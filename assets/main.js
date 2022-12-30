95df524251fa77d5da69f63626eedfd44ea16d16
VitaBar7 <104008965+VitaBar7@users.noreply.github.com>
2022-12-30 10:28:54 +0100

    wording md, style, burger menu wip

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,11 @@ function hashChange() {
     xmlHttp.send( null )
 
 }
+
+let menuBurger = document.querySelector(".menu_burger")
+let sideMenu = document.querySelector(".side_menu")
+
+menuBurger.addEventListener("click", e => {
+    sideMenu.classList.toggle("active")
+
+})
